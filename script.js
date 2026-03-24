5de6fdafd9aa64f1070c22e26bbaca29e0c15155
jxstcallmesanya <veilside7774@gmail.com>
2026-03-24 23:36:51 +0300

    upd

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const config = {
-    auto: 10,  // твои 10 фото
-    people: 5 
+    auto: 10,
+    people: 9
 };
 
 function showGallery(category) {
-    const area = document.getElementById('content-area');
-    area.innerHTML = `<div class="mosaic-grid" id="grid"></div>`;
-    const grid = document.getElementById('grid');
+    const content = document.getElementById('content-area');
+    content.innerHTML = '<div class="mosaic-grid"></div>';
+    const grid = content.querySelector('.mosaic-grid');
 
     for (let i = 1; i <= config[category]; i++) {
         const img = document.createElement('img');
         img.src = `img/${category}/${i}.jpg`;
         grid.appendChild(img);
     }
+    window.scrollTo(0, 0); // Прыгаем вверх при открытии категории
 }
