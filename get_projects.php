7199753b313b2a2f0ab77d470c47876e671b1470
jxstcallmesanya <veilside7774@gmail.com>
2026-03-30 13:49:27 +0300

    upd

--- get_projects.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dir = 'img/projects/';
-$folders = array_filter(glob($dir . '*'), 'is_dir');
-$result = [];
-foreach ($folders as $folder) {
-    $result[] = basename($folder);
-}
-echo json_encode($result);
-?>
